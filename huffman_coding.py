4f67d4f2e8046e82ceb05a8d42b89e796c38738e
Rohan Jakhar <54846603+qRohan@users.noreply.github.com>
2021-03-25 23:35:01 +0530

    Refactored code and improved type hinting in huffman_coding.py
    Improved the type hinting. Code now fully passes static type checkers (tested using pylance on strict settings).
    
    Frequency variable and comparator magic methods separated into it's own class.
    
    removed now redundant import for typing.Optional
    
    Now encoding the file also prints out the compression ratio(uncompressed size/ compressed size) to the console

--- huffman_coding.py
+++ huffman_coding.py
@@ -3,7 +3,6 @@ from collections import Counter
 import os.path
 import heapq
 import pickle
-from typing import Optional
 import sys
 
 
@@ -14,29 +13,36 @@ __author__ = "Rohan Jakhar"
 
 class HuffmanCoding():
     def __init__(self):
-        self.heap: list[HuffmanCoding.HeapLeafNode] = []
+        self.heap: list[HuffmanCoding.HeapNode] = []
         self.codes: dict[str, str] = {}
         self.reverse_mapping: dict[str, str] = {}
 
-    class HeapLeafNode():
-        def __init__(self, char: Optional[str], freq: int):
-            self.char = char
+    class HeapNode():
+        def __init__(self, freq: int):
             self.freq = freq
 
-        def __lt__(self, other: HuffmanCoding.HeapLeafNode):
+        def __lt__(self, other: HuffmanCoding.HeapNode):
             return self.freq < other.freq
 
-        def __eq__(self, other: HuffmanCoding.HeapLeafNode):
+        def __eq__(self, other: HuffmanCoding.HeapNode):
             return self.freq == other.freq
+
+        def __gt__(self, other: HuffmanCoding.HeapNode):
+            return self.freq > other.freq
+
+    class HeapLeafNode(HeapNode):
+        def __init__(self, char: str, freq: int):
+            super().__init__(freq)
+            self.char = char
 
         def __str__(self):
             return f"( {self.char}, {self.freq} )"
 
-    class HeapNode(HeapLeafNode):
-        def __init__(self, freq: int, left_node, right_node):
-            super().__init__(None, freq)
-            self.left: HuffmanCoding.HeapLeafNode = left_node
-            self.right: HuffmanCoding.HeapLeafNode = right_node
+    class HeapInternalNode(HeapNode):
+        def __init__(self, freq: int, left_node: HuffmanCoding.HeapNode, right_node: HuffmanCoding.HeapNode):
+            super().__init__(freq)
+            self.left = left_node
+            self.right = right_node
 
         def __str__(self):
             return f"( freq: {self.freq}, left: {self.left}, right: {self.right} )"
@@ -51,7 +57,8 @@ class HuffmanCoding():
             while(len(self.heap) > 1):
                 left = heapq.heappop(self.heap)
                 right = heapq.heappop(self.heap)
-                merged = self.HeapNode(right.freq + left.freq, left, right)
+                merged = self.HeapInternalNode(
+                    right.freq + left.freq, left, right)
                 heapq.heappush(self.heap, merged)
 
         make_heap(self)
@@ -59,13 +66,13 @@ class HuffmanCoding():
         self.huffman_tree = heapq.heappop(self.heap)
 
     def make_codes(self):
-        def helper(node, curr_code: str):
-            if node.char is not None:
-                self.codes[node.char] = curr_code
-                self.reverse_mapping[curr_code] = node.char
-            else:
+        def helper(node: HuffmanCoding.HeapNode, curr_code: str):
+            if isinstance(node, HuffmanCoding.HeapInternalNode):
                 helper(node.left, curr_code+'0')
                 helper(node.right, curr_code+'1')
+            elif isinstance(node, HuffmanCoding.HeapLeafNode):
+                self.codes[node.char] = curr_code
+                self.reverse_mapping[curr_code] = node.char
 
         helper(self.huffman_tree, "")
 
@@ -89,7 +96,7 @@ class HuffmanCoding():
 
         def decode_text(self: HuffmanCoding):
             curr_code = ""
-            decoded_text_lst = []
+            decoded_text_lst: list[str] = []
             for bit in self.encoded_text:
                 curr_code += bit
                 if curr_code in self.reverse_mapping:
@@ -102,57 +109,59 @@ class HuffmanCoding():
         decode_text(self)
 
     def create_compressed_file(self):
-        pickled = pickle.dumps(self.huffman_tree)
+        pickled = pickle.dumps(self.huffman_tree, protocol=-1)
         size = len(pickled)
         size_bin_str = f"{size:032b}"
         size_lst = [int(size_bin_str[index: index+8], 2)
                     for index in range(0, 32, 8)]
         size_bytes = bytes(size_lst)
 
-        file_name = self.path+".bin"
+        file_name: str = self.path+".bin"
         with open(file_name, "wb+") as handle:
             handle.write(size_bytes)
             handle.write(pickled)
             handle.write(self.eptext_bytes)
+        self.compressed_size = os.path.getsize(file_name)
 
-    def read_compressed_file(self, path):
+    def read_compressed_file(self, path: str):
         absolue_path = os.path.abspath(path)
         with open(absolue_path, "rb") as cfhandle:
             base_name = os.path.basename(absolue_path)
-            self.path, ext = os.path.splitext(base_name)
+            self.path: str = os.path.splitext(base_name)[0]
 
-            # can be done using direct math
+            # can be using direct math
             tree_size_bin = ''.join(
                 [f"{byte:08b}" for byte in cfhandle.read(4)])
             tree_size = int(tree_size_bin, 2)
 
             pickled_tree = cfhandle.read(tree_size)
-            self.huffman_tree: HuffmanCoding.HeapLeafNode = pickle.loads(
+            self.huffman_tree: HuffmanCoding.HeapNode = pickle.loads(
                 pickled_tree)
 
             bit_string_list = [f"{byte:08b}" for byte in cfhandle.read()]
         self.encoded_padded_text = ''.join(bit_string_list)
 
-    def read_uncompressed_file(self, path):
+    def read_uncompressed_file(self, path: str):
         absolue_path: str = os.path.abspath(path)
+        self.uncompressed_size = os.path.getsize(absolue_path)
         with open(absolue_path, 'r') as fhandle:
             self.text = fhandle.read()
             base_name = os.path.basename(absolue_path)
-            self.path, ext = os.path.splitext(base_name)
+            self.path, _ = os.path.splitext(base_name)
 
     def create_uncompressed_file(self):
-        file_name = self.path+"_uncompressed.txt"
+        file_name: str = self.path+"_uncompressed.txt"
         with open(file_name, 'w') as fhandle:
             fhandle.write(self.text)
 
-    def encode(self, path):
+    def encode(self, path: str):
         self.read_uncompressed_file(path)
         self.make_tree()
         self.make_codes()
         self.encode_text()
         self.create_compressed_file()
 
-    def decode(self, path):
+    def decode(self, path: str):
         self.read_compressed_file(path)
         self.make_codes()
         self.decode_eptext()
@@ -166,8 +175,10 @@ if __name__ == "__main__":
     method = method.lower()
     if method == 'e' or method == 'encode':
         x.encode(file_name)
+        print(f"Compression ratio: {x.uncompressed_size / x.compressed_size}")
+
     elif method == 'd' or method == 'decode':
         x.decode(file_name)
     else:
-        print("invalid method")
+        print("Invalid method")
         sys.exit(0)
